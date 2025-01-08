1c6fc8324cee1187b3082fc0ea84a21d2ce9ed2b
Juan Melgar <ranagha@gmail.com>
2025-01-08 20:56:11 +0100

    Bishop movements

--- src/chess/Application/Model/Bishop.py
+++ src/chess/Application/Model/Bishop.py
@@ -1,3 +1,5 @@
+from idlelib.outwin import file_line_pats
+
 from src.chess.Application.Model.Pieza import Pieza
 
 class Bishop(Pieza):
@@ -5,4 +7,29 @@ class Bishop(Pieza):
         super().__init__(color, fila, columna)
 
     def possible_moves(self):
-        return []
+        movements = []
+        filai = self.fila()
+        columnai = self.columna()
+        while filai < 7 and columnai < 7:
+            filai = filai + 1
+            columnai = columnai + 1
+            movements.append((filai, columnai))
+        filai = self.fila()
+        columnai = self.columna()
+        while filai < 7 and columnai > 0:
+            filai = filai + 1
+            columnai = columnai -1
+            movements.append((filai, columnai))
+        filai = self.fila()
+        columnai = self.columna()
+        while filai > 0 and columnai < 7:
+            filai = filai - 1
+            columnai = columnai + 1
+            movements.append((filai, columnai))
+        filai = self.fila()
+        columnai = self.columna()
+        while filai > 0 and columnai > 0:
+            filai = filai - 1
+            columnai = columnai - 1
+            movements.append((filai, columnai))
+        return movements
